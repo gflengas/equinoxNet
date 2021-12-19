478b1d00c1548ee793e4e0a00fe45f081860ca8d
Georgios Flengas <53496656+gflengas@users.noreply.github.com>
2021-12-20 00:35:48 +0200

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/deviltest.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/giorgosflg/CLionProjects/deviltest/connected_layer.c" "C:/Users/giorgosflg/CLionProjects/deviltest/cmake-build-debug/CMakeFiles/deviltest.dir/connected_layer.c.obj"
+  "C:/Users/giorgosflg/CLionProjects/deviltest/conv_layer.c" "C:/Users/giorgosflg/CLionProjects/deviltest/cmake-build-debug/CMakeFiles/deviltest.dir/conv_layer.c.obj"
+  "C:/Users/giorgosflg/CLionProjects/deviltest/custom_math.c" "C:/Users/giorgosflg/CLionProjects/deviltest/cmake-build-debug/CMakeFiles/deviltest.dir/custom_math.c.obj"
+  "C:/Users/giorgosflg/CLionProjects/deviltest/data.c" "C:/Users/giorgosflg/CLionProjects/deviltest/cmake-build-debug/CMakeFiles/deviltest.dir/data.c.obj"
+  "C:/Users/giorgosflg/CLionProjects/deviltest/main.c" "C:/Users/giorgosflg/CLionProjects/deviltest/cmake-build-debug/CMakeFiles/deviltest.dir/main.c.obj"
+  "C:/Users/giorgosflg/CLionProjects/deviltest/maxpool_layer.c" "C:/Users/giorgosflg/CLionProjects/deviltest/cmake-build-debug/CMakeFiles/deviltest.dir/maxpool_layer.c.obj"
+  "C:/Users/giorgosflg/CLionProjects/deviltest/network.c" "C:/Users/giorgosflg/CLionProjects/deviltest/cmake-build-debug/CMakeFiles/deviltest.dir/network.c.obj"
+  "C:/Users/giorgosflg/CLionProjects/deviltest/softmax_layer.c" "C:/Users/giorgosflg/CLionProjects/deviltest/cmake-build-debug/CMakeFiles/deviltest.dir/softmax_layer.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/deviltest.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/deviltest.dir/connected_layer.c.obj"
+  "CMakeFiles/deviltest.dir/conv_layer.c.obj"
+  "CMakeFiles/deviltest.dir/custom_math.c.obj"
+  "CMakeFiles/deviltest.dir/data.c.obj"
+  "CMakeFiles/deviltest.dir/main.c.obj"
+  "CMakeFiles/deviltest.dir/maxpool_layer.c.obj"
+  "CMakeFiles/deviltest.dir/network.c.obj"
+  "CMakeFiles/deviltest.dir/softmax_layer.c.obj"
+  "deviltest.exe"
+  "deviltest.exe.manifest"
+  "deviltest.pdb"
+  "libdeviltest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/deviltest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
